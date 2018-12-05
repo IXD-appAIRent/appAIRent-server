20818b62ebbc8e0e6565517cd948d9224810ec11
Julian Pfeifer <julian.pfeifer.88@gmail.com>
2018-12-05 13:52:39 +0100

    initial spring boot project

---
+++ src/main/kotlin/org/codingixd/appairent/server/TestServer.kt
@@ -0,0 +1,28 @@
+package org.codingixd.appairent.server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+class GreetingController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+        Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
